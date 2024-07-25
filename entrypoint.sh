b48598bc6fe63b7adfb3d64c426208c65abbb6c3
Emile Nijssen <emile@athom.nl>
2024-07-25 10:40:32 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 cd /github/workspace/
 
 npm ci --ignore-scripts
-HOMEY_HEADLESS="1" HOMEY_HOMEY_PAT="$1" npx homey app publish
+HOMEY_HEADLESS="1" HOMEY_PAT="$1" npx homey app publish
 
 echo "url=https://tools.developer.homey.app/apps/app/$(cat app.json | jq --raw-output .id)" >> $GITHUB_OUTPUT
