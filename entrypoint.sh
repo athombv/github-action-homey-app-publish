9688674fc32b8b73575217027b03db84fd2a0ecf
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-07-30 00:32:10 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 
 cd /github/workspace/
 
-npm --userconfig $NPM_CONFIG_USERCONFIG ci --ignore-scripts
+cat $NPM_CONFIG_USERCONFIG
+
+npm ci --ignore-scripts
 HOMEY_HEADLESS="1" HOMEY_PAT="$1" npx homey app publish
 
 echo "url=https://tools.developer.homey.app/apps/app/$(cat app.json | jq --raw-output .id)" >> $GITHUB_OUTPUT
