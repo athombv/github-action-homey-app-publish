7ccb4b8cdaa52bb5507b287c746b6c87ce4c0896
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-07-30 00:13:03 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 cd /github/workspace/
 
-npm set npm config set userconfig $NPM_CONFIG_USERCONFIG
+npm config set userconfig $NPM_CONFIG_USERCONFIG
 npm ci --ignore-scripts
 HOMEY_HEADLESS="1" HOMEY_PAT="$1" npx homey app publish
 
