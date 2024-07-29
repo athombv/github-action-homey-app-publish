259212a3dd9abd815de6b68e035eee6f1afb1eb4
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-07-30 01:13:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,13 @@ set -e
 
 cd /github/workspace/
 
+if [ -f "/home/runner/work/_temp/.npmrc" ]; then
+    echo "Contenu du fichier .npmrc:"
+    cat /home/runner/work/_temp/.npmrc
+else
+    echo "Le fichier .npmrc n'existe pas dans le répertoire spécifié."
+fi
+
 npm ci --ignore-scripts
 HOMEY_HEADLESS="1" HOMEY_PAT="$1" npx homey app publish
 
