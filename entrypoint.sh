280ab39c07ce41945d2f3a7511d74d7fe46dd54c
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-07-30 00:52:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,10 @@ set -e
 
 cd /github/workspace/
 
-cp $NPM_CONFIG_USERCONFIG /github/workspace/.npmrc
+cp $NPM_CONFIG_USERCONFIG .npmrc
+docker build --build-arg NODE_AUTH_TOKEN .
 
 npm ci --ignore-scripts
 HOMEY_HEADLESS="1" HOMEY_PAT="$1" npx homey app publish
-
-rm /github/workspace/.npmrc
 
 echo "url=https://tools.developer.homey.app/apps/app/$(cat app.json | jq --raw-output .id)" >> $GITHUB_OUTPUT
