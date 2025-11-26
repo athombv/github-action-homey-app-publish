22d6419c50f54e7605273b625e9123df9653ce82
Herman Meerlo <herman.meerlo@athom.nl>
2025-11-26 11:36:26 +0100

    Revert "Moved envs from inline to export, as the runner is not using the envs"
    This reverts commit a17806061f22e4af7235e8adb8b3d6ce6910df19.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ set -e
 cd /github/workspace/
 
 npm ci --ignore-scripts
-export HOMEY_HEADLESS="1"
-export HOMEY_PAT="$1"
-npx homey app publish
+HOMEY_HEADLESS="1" HOMEY_PAT="$1" npx homey app publish
 
 echo "url=https://tools.developer.homey.app/apps/app/$(cat app.json | jq --raw-output .id)" >> $GITHUB_OUTPUT
